9ad4b38c05f55a3f16a365175330aef2ac5a33da
Di <abakarova.diana99@gmail.com>
2021-04-07 13:07:40 +0300

    first step

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Footer } from './components/Footer/Footer';
+import './App.css';
+import { Header } from "./components/Header/Header";
+import { BlogContent } from './components/BlogContent/BlogContent';
+
+export function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <BlogContent />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
