072cbf04eac45593550687719f2c87293a1f6f59
Sey-Uki <abakarova.diana99@gmail.com>
2021-08-10 15:26:44 +0300

    making LoginPage

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import { Footer } from "./components/Footer/Footer";
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { BlogContent } from "./components/BlogContent/BlogContent";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage/LoginPage";
 
 export function App() {
   return (
-    <div className="App">
-      <Header />
-      <main>
-        <BlogContent />
-      </main>
-
-      <Footer year={new Date().getFullYear()} />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <BlogContent />
+            </Route>
+            <Route exact path="/login">
+              <LoginPage />
+            </Route>
+          </Switch>
+        </main>
+        <Footer year={new Date().getFullYear()} />
+      </div>
+    </BrowserRouter>
   );
 }
